2e778e18ee3a104b3461ae0f77b7b77fc845d702
Sonny Mayta <mayta.sonny@gmail.com>
2025-03-06 10:50:27 -0400

    Watchtower

---
+++ Watchtower/Watchtower/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Watchtower
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("X: ");
+            int x = int.Parse(Console.ReadLine());
+            Console.Write("Y: ");
+            int y = int.Parse(Console.ReadLine());
+
+            if (x == 0 && y == 0) Console.WriteLine("The enemy is here");
+            if (x == 0 && y > 0) Console.WriteLine("The enemy is in the N");
+            if (x == 0 && y < 0) Console.WriteLine("The enemy is in the S");
+            if (x > 0 && y == 0) Console.WriteLine("The enemy is in the E");
+            if (x < 0 && y == 0) Console.WriteLine("The enemy is in the W");
+            if (x > 0 && y > 0) Console.WriteLine("The enemy is in the NE");
+            if (x > 0 && y < 0) Console.WriteLine("The enemy is in the SE");
+            if (x < 0 && y > 0) Console.WriteLine("The enemy is in the NW");
+            if (x < 0 && y < 0) Console.WriteLine("The enemy is in the SW");
+        }
+    }
+}
