1f72d87cd9d40f1c83e5b9757adaab71304c08b8
Sonny Mayta <mayta.sonny@gmail.com>
2025-03-06 10:49:57 -0400

    RepairingTheClocktower

---
+++ RepairingTheClocktower/RepairingTheClocktower/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace RepairingTheClocktower
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+            Console.Write("Enter a number: ");            
+            try
+            {
+                number = int.Parse(Console.ReadLine());
+            }
+            catch (Exception)
+            {
+                number = 0;
+                throw;
+            }
+            
+            if (number % 2 == 0)
+            {
+                Console.WriteLine("Tick");
+            }
+            else
+            {
+                Console.WriteLine("Tock");
+            }
+        }
+    }
+}
