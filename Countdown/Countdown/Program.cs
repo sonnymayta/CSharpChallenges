d80ce89f99d2f2119531a495dfbc4237d6addac6
Sonny Mayta <mayta.sonny@gmail.com>
2025-03-19 09:52:46 -0400

    Countdown

---
+++ Countdown/Countdown/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Countdown
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintCountdown(10);
+
+            void PrintCountdown(int n)
+            {
+                if (n == 1) Console.WriteLine("1");
+                else
+                {
+                    Console.WriteLine(n);
+                    PrintCountdown(n - 1);
+                }
+            }
+        }        
+    }
+}
