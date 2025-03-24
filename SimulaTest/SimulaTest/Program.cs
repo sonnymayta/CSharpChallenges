bb1c0439159a2b3df8ce1489089e80af6763f539
Sonny Mayta <mayta.sonny@gmail.com>
2025-03-24 15:48:50 -0400

    SimulaTest

---
+++ SimulaTest/SimulaTest/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace SimulaTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string userAction;
+            bool active = true;
+
+            StateChest current = StateChest.Locked;
+
+            do
+            {
+                Console.Write($"The chest is {current}. What do you want to do? ");
+                userAction = Console.ReadLine();
+                if (userAction == "open")
+                {
+                    if (current == StateChest.Unlocked)
+                    {
+                        current = StateChest.Open;
+                    }
+                }
+                else if (userAction == "close")
+                {
+                    if (current == StateChest.Open)
+                    {
+                        current = StateChest.Unlocked;
+                    }
+                }
+                else if (userAction == "lock")
+                {
+                    if (current == StateChest.Unlocked)
+                    {
+                        current = StateChest.Locked;
+                    }
+                }
+                else if (userAction == "unlock")
+                {
+                    if (current == StateChest.Locked)
+                    {
+                        current = StateChest.Unlocked;
+                    }
+                }
+                else { active = false; }
+            } while (active);
+        }
+
+        enum StateChest { Open, Locked, Unlocked };
+    }
+}
