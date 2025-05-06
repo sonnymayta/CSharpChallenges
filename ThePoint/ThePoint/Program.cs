1fdf82252f46714ba70a2b543554732a0c6749b3
Sonny Mayta <mayta.sonny@gmail.com>
2025-05-06 11:17:02 -0400

    ThePoint

---
+++ ThePoint/ThePoint/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace ThePoint
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Point firstPoint = new(2, 3);
+            Point secondPoint = new(-4, 0);
+            firstPoint.GetCoordinates();
+            secondPoint.GetCoordinates();
+        }
+    }
+
+    internal class Point
+    {
+        internal int X { get; set; }
+
+        internal int Y { get; set; }
+
+        internal Point(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        internal Point()
+        {
+            X = 0;
+            Y = 0;
+        }
+
+        internal void GetCoordinates()
+        {
+            Console.WriteLine($"({X}, {Y})");
+        }
+    }
+}
