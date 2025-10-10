2ce4c6cbb8547ab2b8e92a4c5a8daa1cfce6b297
Sonny Mayta <mayta.sonny@gmail.com>
2025-10-10 10:52:09 -0400

    ColoredItems

---
+++ ColoredItems/ColoredItems/ColoredItem.cs
@@ -0,0 +1,20 @@
+﻿namespace ColoredItems
+{
+    internal class ColoredItem<T>
+    {
+        public T Item { get; }
+        public ConsoleColor Color { get; }
+
+        public ColoredItem(T item, ConsoleColor color)
+        {
+            Item = item;
+            Color = color;
+        }
+
+        public void Display()
+        {
+            Console.ForegroundColor = Color;
+            Console.WriteLine(Item);
+        }
+    }
+}
---
+++ ColoredItems/ColoredItems/Program.cs
@@ -0,0 +1,9 @@
+﻿using ColoredItems;
+
+ColoredItem<Sword> sword = new ColoredItem<Sword>(new Sword(), ConsoleColor.Blue);
+ColoredItem<Bow> bow = new ColoredItem<Bow>(new Bow(), ConsoleColor.Red);
+ColoredItem<Axe> axe = new ColoredItem<Axe>(new Axe(), ConsoleColor.Green);
+
+sword.Display();
+bow.Display();
+axe.Display();
