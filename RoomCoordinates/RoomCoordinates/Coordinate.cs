6cd54b5edfdaa26689468edb6dc12ceba99aa911
Sonny Mayta <mayta.sonny@gmail.com>
2025-10-10 10:49:15 -0400

    RoomCoordinates

---
+++ RoomCoordinates/RoomCoordinates/Coordinate.cs
@@ -0,0 +1,24 @@
+﻿namespace RoomCoordinates
+{
+    struct Coordinate
+    {
+        public int Row { get; }
+        public int Column { get; }
+
+        public Coordinate(int row, int column)
+        {
+            Row = row;
+            Column = column;
+        }
+
+        public static bool AreAdjacent (Coordinate a, Coordinate b)
+        {
+            int rowChange = a.Row - b.Row;
+            int columnChange = a.Column - b.Column;
+
+            if (Math.Abs(rowChange) <= 1 && columnChange == 0) return true;
+            if (Math.Abs(columnChange) <= 1 && rowChange == 0) return true;
+            return false;
+        }
+    }
+}
---
+++ RoomCoordinates/RoomCoordinates/Program.cs
@@ -0,0 +1,9 @@
+﻿using RoomCoordinates;
+
+Coordinate a = new Coordinate(3, 3);
+Coordinate b = new Coordinate(2, 3);
+Coordinate c = new Coordinate(2, 2);
+
+Console.WriteLine(Coordinate.AreAdjacent(a, b));
+Console.WriteLine(Coordinate.AreAdjacent(b, c));
+Console.WriteLine(Coordinate.AreAdjacent(a, c));
