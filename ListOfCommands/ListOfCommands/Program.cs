fa9da3c4907c38895ff4336a77a0729e3ca8e91d
Sonny Mayta <mayta.sonny@gmail.com>
2025-10-15 10:39:30 -0400

    ListOfCommands

---
+++ ListOfCommands/ListOfCommands/Program.cs
@@ -0,0 +1,54 @@
+﻿using ListOfCommands;
+
+Robot robot = new Robot();
+
+while (true)
+{
+    string? input = Console.ReadLine();
+
+    if (input == "stop") break;
+
+    robot.Commands.Add(input switch
+    {
+        "on" => new OnCommand(),
+        "off" => new OffCommand(),
+        "north" => new NorthCommand(),
+        "south" => new SouthCommand(),
+        "east" => new EastCommand(),
+        "west" => new WestCommand(),
+    });
+}
+
+Console.WriteLine();
+
+robot.Run();
+
+public class OffCommand : IRobotCommand
+{
+    public void Run(Robot robot) => robot.IsPowered = false;
+}
+
+public class OnCommand : IRobotCommand
+{
+    public void Run(Robot robot) => robot.IsPowered = true;
+}
+
+public class WestCommand : IRobotCommand
+{
+    public void Run(Robot robot) { if (robot.IsPowered) robot.X--; }
+}
+
+public class EastCommand : IRobotCommand
+{
+    public void Run(Robot robot) { if (robot.IsPowered) robot.X++; }
+}
+
+public class SouthCommand : IRobotCommand
+{
+    public void Run(Robot robot) { if (robot.IsPowered) robot.Y--; }
+}
+
+public class NorthCommand : IRobotCommand
+{
+    public void Run(Robot robot) { if (robot.IsPowered) robot.Y++; }
+}
