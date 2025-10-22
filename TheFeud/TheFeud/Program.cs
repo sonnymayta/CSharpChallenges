04ac3d359073aaee13998dc156256066c7e63295
Sonny Mayta <mayta.sonny@gmail.com>
2025-10-22 15:46:16 -0400

    TheFeud

---
+++ TheFeud/TheFeud/Program.cs
@@ -0,0 +1,20 @@
+﻿using IField;
+using McDroid;
+using McPig = McDroid.Pig;
+
+Sheep sheep = new Sheep();
+Cow cow = new Cow();
+IField.Pig pigOne = new IField.Pig();
+McPig pigTwo = new McPig();
+
+namespace IField
+{
+    public class Sheep { }
+    public class Pig { }
+}
+
+namespace McDroid
+{
+    public class Cow { }
+    public class Pig { }
+}
